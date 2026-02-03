9eda1623378450b3cda560e3119fa4d25b28cd0b
Maleshkumar.V <v.maleshkumar26032006@gmail.com>
2026-02-03 08:12:29 +0530

    Add sumdiff.c for basic arithmetic operations

---
+++ sumdiff.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a, b, e, f;
+    float c, d, g, h;
+    scanf("%d %d", &a, &b);
+    scanf("%f %f", &c, &d);
+    e = a + b;
+    f = a - b;
+    g = c + d;
+    h = c - d;
+    printf("%d %d\n", e, f);
+    printf("%.1f %.1f\n", g, h);
+    return 0;
+}
